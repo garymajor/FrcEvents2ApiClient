1f42927f5cbd46b758aa61429a758592509af576
Gary Major <garymajor@hotmail.com>
2016-03-22 16:23:03 -0700

    First commit

---
+++ FrcEvent2ApiClient/FrcEvent2ApiClient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Web.Http;
+
+namespace FrcEvent2ApiClient
+{
+    public static class FrcEvent2ApiClient
+    {
+        const string uriBase = "http://";
+
+        public static void GetTeamData()
+        {
+            var teamNumber = "2147";
+            var url = uriBase + "";
+
+            var client = new HttpClient();
+        }
+
+        public static void GetEventRankings()
+        {
+
+        }
+
+        public static void GetMatchesForTeam()
+        {
+            var teamNumber = "2147";
+
+        }
+
+        public static void GetMatchesForEvent()
+        {
+            var e = "WAELL"; // Washington Ellensburg
+        }
+    }
+}
